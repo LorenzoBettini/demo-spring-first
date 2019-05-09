691db75eac3ff574c4e4405938e49600871c38ee
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-09 22:12:23 +0200

    RepositoryInitializer

--- src/main/java/com/examples/spring/demo/first/DemoSpringFirstApplication.java
+++ src/main/java/com/examples/spring/demo/first/DemoSpringFirstApplication.java
@@ -1,7 +1,13 @@
 package com.examples.spring.demo.first;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import com.examples.spring.demo.first.model.Person;
+import com.examples.spring.demo.first.repository.PersonRepository;
 
 @SpringBootApplication
 public class DemoSpringFirstApplication {
@@ -10,4 +16,16 @@ public class DemoSpringFirstApplication {
 		SpringApplication.run(DemoSpringFirstApplication.class, args);
 	}
 
+	@Component
+	static class RepositoryInitializer implements CommandLineRunner {
+		@Autowired
+		private PersonRepository personRepository;
+		
+		@Override
+		public void run(String... args) throws Exception {
+			personRepository.save(new Person("Mario", "Rossi"));
+			personRepository.save(new Person("Carlo", "Rossi"));
+			personRepository.save(new Person("Paolo", "Bianchi"));
+		}
+	}
 }
