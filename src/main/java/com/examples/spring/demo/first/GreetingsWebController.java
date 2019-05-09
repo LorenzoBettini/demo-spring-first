350883b9919df4327ffb5afd58faa3fe5f4dacff
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-05-09 09:46:58 +0200

    @Value inject of a property

--- src/main/java/com/examples/spring/demo/first/GreetingsWebController.java
+++ src/main/java/com/examples/spring/demo/first/GreetingsWebController.java
@@ -2,15 +2,20 @@ package com.examples.spring.demo.first;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GreetingsWebController {
 
+	// injected value through a property
+	@Value("${welcome.message:World!}")
+	private String welcomeMessage;
+
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
-		model.put("message", "World");
+		model.put("message", welcomeMessage);
 		return "index";
 	}
 }
